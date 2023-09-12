147a32fab418aa159c8f0c0238860a6234d280bf
Bogdan.Syvulyak <Bogdan.Syvulyak@ukr.net>
2023-09-12 16:06:43 +0300

    add mutable collections

--- src/main/scala/net/study/functional/lesson_12_mutable_collection/MutableCollections.scala
+++ src/main/scala/net/study/functional/lesson_12_mutable_collection/MutableCollections.scala
@@ -1,6 +1,8 @@
 package net.study.functional.lesson_12_mutable_collection
 
+import scala.annotation.tailrec
 import scala.collection.mutable
+import scala.language.postfixOps
 
 object MutableCollections extends App {
 
@@ -65,7 +67,7 @@ object MutableCollections extends App {
 
   println(s"array: $arraySeq")
 
-  // ArrayBuffer
+  // ArrayBuffer (immutable version Vector)
 
   val arrayBuffer = mutable.ArrayBuffer(1, 2, 3, 4, 5, 6, 7, 8, 9)
 
@@ -92,9 +94,9 @@ object MutableCollections extends App {
 
   //Removing
 
- // arrayBuffer -= 2
+  // arrayBuffer -= 2
 
-   // by
+  // by
   arrayBuffer --= Seq(100, 1)
 
   println(s"arrayBuffer with removing by values: $arrayBuffer")
@@ -104,5 +106,96 @@ object MutableCollections extends App {
   arrayBuffer remove(0, 2)
 
   println(s"arrayBuffer with removing by index 2: $arrayBuffer")
+
+
+  /// Queue (there is also immutable version) // FIFO
+
+  val simpleQueue = mutable.Queue(1, 2, 3, 4, 5, 6, 7, 8)
+
+  val peekResult = simpleQueue.headOption // safe, return Option
+
+  val frontResult = simpleQueue.front // dangerous
+
+  //val dequeueResult = simpleQueue.dequeue() //poll
+
+  // enqueue
+
+  //single
+  simpleQueue.enqueue(100)
+
+  // multiple
+
+  simpleQueue.enqueue(200, 300, 400, 500, 600)
+
+
+  /*  simpleQueue.foreach{ elem =>
+
+      println(s"next Elem: $elem")
+    }*/
+
+  // while (simpleQueue.nonEmpty) println(s"next Elem: ${simpleQueue.dequeue()}")
+
+  def dequeueAll[T](anyQueue: mutable.Queue[T], acc: T)(accFunc: (T, T) => T): T = {
+    @tailrec
+    def dequeue(anyQueue: mutable.Queue[T], acc: T): T = {
+      val next = anyQueue.dequeue()
+      // println(s"next: $next")
+      if (anyQueue.isEmpty) accFunc(acc, next) else dequeue(anyQueue, accFunc(acc, next))
+    }
+
+    dequeue(anyQueue, acc)
+  }
+
+  val dequeueAllResult = dequeueAll(simpleQueue, 0)((acc, next) => acc + next)
+
+  println(s"dequeueAllResult: $dequeueAllResult")
+
+  case class MyNumber(myInt: Int)
+
+  implicit val myNumberOrdering: Ordering[MyNumber] = Ordering.fromLessThan((l, r) => (l.myInt compare r.myInt) > 0) //Ordering.by(_.myInt)
+
+  val priorityQueue: mutable.PriorityQueue[MyNumber] = mutable.PriorityQueue[MyNumber](MyNumber(2), MyNumber(1), MyNumber(3))
+
+  // val dequeuePriorityQueueAllResult = dequeueAll[Int](priorityQueue, 0, (acc, next) => acc + next )
+
+  //val seq: Seq[MyNumber] = priorityQueue.dequeueAll
+
+  val priorityQueueAcc: Int = priorityQueue.foldLeft(0)((acc, next) => {
+    println(s"Next priority: $next")
+    acc + next.myInt
+  })
+
+  //println(priorityQueueAcc)
+
+  // Stack LIFO
+
+  val simpleStack = mutable.Stack(1, 2, 3)
+
+  // put into stack
+
+  simpleStack.push(100)
+
+  // get last from stack
+  println(simpleStack.pop)
+
+  println(simpleStack.headOption.map(_ => simpleStack pop))
+
+  println(simpleStack.headOption.map(_ => simpleStack pop))
+
+  println(simpleStack.headOption.map(_ => simpleStack pop))
+
+  println(simpleStack.headOption.map(_ => simpleStack pop))
+
+  // StringBuilder
+
+  val stringBuilder = new mutable.StringBuilder("Example of ")
+
+  stringBuilder ++= "concatenation"
+
+  println(stringBuilder)
+
+  // Sets
+
+  val mutableSet = mutable.Set(1, 4, 7)
 
 }
